c1845a2a09332426b0e4c2aee243e722375edeb2
Swetha2021g <93753038+Swetha2021g@users.noreply.github.com>
2021-11-04 23:05:51 -0500

    Add files via upload

---
+++ MyFrame.java
@@ -0,0 +1,24 @@
+package Simulation;
+
+import java.awt.Image;
+import java.awt.Toolkit;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.SwingContainer;
+
+public class MyFrame extends JFrame{
+    MyPanel panel = new MyPanel();
+    public MyFrame() {
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setTitle("Simulator");
+        add(panel);
+        setSize(800,800);
+        setLocationRelativeTo(null);
+        pack();
+        setVisible(true);
+    }
+
+}
